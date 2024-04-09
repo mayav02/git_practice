2c5284b300e1debab9b8fd2f33701da5688150c1
Maya Vateva <maya.vateva02@gmail.com>
2024-04-09 15:14:14 -0700

    assign variables for card values

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Card:
     def __init__(self, suit, val):
         self.suit = suit
@@ -7,6 +8,13 @@ class Card:
 
     def show(self):
         print("{} of {}".format(self.value, self.suit))
+
+    def blackjack_value(self):
+        if self.value in range(11,14):
+            return 10
+        else:
+            return self.value
+        
 
 class Deck:
     def __init__(self):
@@ -31,47 +39,71 @@ class Deck:
     def drawCard(self):
         return self.cards.pop()
     
-bank = 500
-min_bet = 1
+class Play:
+    def __init__(self):
+        self.bank = 500
+        self.min_bet = 1
 
-while bank >= 500:
-    print("Welcome to Blackjack!")    
-    player = input(f"You are starting with ${bank}. Would you like to play a hand? ")
-    if player == 'yes' and bank > 0:
-        bet = int(input("Place your bet: "))
-        if bet < min_bet:
-            print("The minimum is $1. ")
-        if bet >= min_bet:
-            bank =- bet
-            deck = Deck()
-            deck.build()
-            deck.shuffle()
-            for _ in range(1):
-                print("You are dealt: ")
-                for _ in range(2):
-                    deck.drawCard()
-                    deck.show()
-            for _ in range(1):
-                print("The dealer is dealt: " )
-                for _ in range(1):
-                    deck.drawCard()
-                    deck.show()
-                    print("Unknown")
-            first_hit_or_stay = input("Would you like to hit or stay?")
-            if first_hit_or_stay == 'hit':
-                    for _ in range(1):
+    def play_black(self):
+        while self.bank >= 500:
+            print("Welcome to Blackjack!")    
+            player = input(f"You are starting with ${bank}. Would you like to play a hand? ")
+            if player == 'yes' and bank > 0:
+                bet = int(input("Place your bet: "))
+                if bet < self.min_bet:
+                    print("The minimum is $1. ")
+                if bet > self.mbank:
+                    print("You don't have enough money for that bet.")
+                else:
+                    bank -= bet
+                    # Creating an instance of the class.
+                    deck = Deck()
+                    deck.shuffle()
+                    player_hand = []
+                    dealer_hand = []
+
+                    for _ in range(2):
+                        player_hand.append(deck.drawCard())
+                        dealer_hand.append(deck.drawCard())
+
+
+                        player_total = sum(card.blackjack_value() for card in player_hand)
                         print("You are dealt: ")
-                        for _ in range(2):
-                            deck.drawCard()
-                            deck.show()
-            if first_hit_or_stay == 'stay':
-                for _ in range(1):
-                    print("The dealer is dealt: " )
+                        for card in player_hand:
+                            card.show()
+                        print("Total value of your hand:", player_total)
+                    
+                    dealer_total = sum(card.blackjack_value() for card in dealer_hand)
+                    print("Dealer's cards:")
+                    dealer_hand[0].show()
+                    print("Unknown")
+                    print("Total value of dealer's hand:", dealer_hand[0].blackjack_value())
+
                     for _ in range(1):
-                        for _ in range(2):
+                        print("The dealer is dealt: " )
+                        for _ in range(1):
                             deck.drawCard()
                             deck.show()
-                
-    if player == 'no':
-        print("Okay byeeeee!")
+                            print("Unknown")
+                    first_hit_or_stay = input("Would you like to hit or stay?")
+                    if first_hit_or_stay == 'hit':
+                            for _ in range(1):
+                                print("You are dealt: ")
+                                for _ in range(1):
+                                    deck.drawCard()
+                                deck.show()
+                            for _ in range(2):
+                                print("The dealer is dealt: " )
+                                for _ in range(1):
+                                    deck.drawCard()
+                                    deck.show()
+                    if first_hit_or_stay == 'stay':
+                        print("locked")
+                    deck.drawCard()
+                    deck.show()
+
+            if player == 'no':
+                print("Okay byeeeee!")
+
+Play()
         
